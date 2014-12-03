783f83455ee9c652cd0b05487957cc9d0e07b4a4
daniel anggrianto <danggrianto@50onred.com>
2014-12-03 16:59:55 -0500

    setup: bump version 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='krypton',
-    version='0.1.1',
+    version='0.1.2',
     url='https://bitbucket.org/danggrianto/krypton',
     license='BSD',
     author='Daniel Anggrianto',
