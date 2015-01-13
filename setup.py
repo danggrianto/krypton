6221c18c5ec24c9ff65d75344762c7a2ef2a7145
daniel anggrianto <danggrianto@50onred.com>
2015-01-13 15:15:55 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='kryptonium',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/danggrianto/kryptonium',
     license='GNU GPL v2.0',
     author='Daniel Anggrianto',
